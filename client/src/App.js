8a957c3d8b867b3956395f97d45e88b0d0b5fbcc
chefkuna <sukhyun94@naver.com>
2022-10-27 00:13:35 +0900

    send message

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "ㅎㅇ요" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Message..." />
+      <button onClick={sendMessage}>Send message</button>
     </div>
   );
 }
