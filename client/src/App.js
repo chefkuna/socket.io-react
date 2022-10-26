8b1d54eafd6e9f5427b1f4c7bb1cfecf231a561d
chefkuna <sukhyun94@naver.com>
2022-10-27 00:35:23 +0900

    join room

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,18 @@ import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
   const sendMessage = () => {
-    socket.emit("send_message", { message });
+    socket.emit("send_message", { message, room });
   };
 
   useEffect(() => {
@@ -19,6 +27,13 @@ function App() {
 
   return (
     <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join Room</button>
       <input
         placeholder="Message..."
         onChange={(e) => {
